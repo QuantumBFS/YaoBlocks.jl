05a2f4051fb13b68a9eab31fdbba1f0921763f3c
Roger-luo <hiroger@qq.com>
2019-05-12 21:16:14 +0800

    add test

--- test/abstract_blocks.jl
+++ test/abstract_blocks.jl
@@ -4,3 +4,9 @@ using Test, YaoBlocks
     @test getiparams(phase(0.1)) == 0.1
     @test getiparams(2 * phase(0.1)) == ()
 end
+
+@testset "block to matrix conversion" begin
+    for each in [X, Y, Z, H]
+        Matrix{ComplexF64}(each) == Matrix{ComplexF64}(mat(each))
+    end
+end
