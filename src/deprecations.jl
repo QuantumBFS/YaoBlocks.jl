5a68bb3f5c8e0dbef5988ea22b97d2daf5771041
GiggleLiu <cacate0129@gmail.com>
2019-07-19 11:42:08 +0800

    add deprecation warn

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -4,3 +4,5 @@
 @deprecate niparameters(args...) niparams(args...)
 @deprecate parameter_type(args...) parameters_eltype(args...)
 const Sum = Add
+@deprecate Sum(args...) Add(args...)
+@deprecate Add(blocks::AbstractVector{<:AbstractBlock{N}}) where {N} Add{N}(blocks)
