d691084482ecc00276b5b129363097952d10371c
Leo <cacate0129@gmail.com>
2019-07-31 05:20:12 +0800

    Update abstract_block.jl

--- src/abstract_block.jl
+++ src/abstract_block.jl
@@ -24,7 +24,7 @@ end
 
 _apply_fallback!(r::AbstractRegister, b::AbstractBlock) = throw(NotImplementedError(:_apply_fallback!, (r, b)))
 
-function _apply_fallback!(r::ArrayReg, b::AbstractBlock)
+function _apply_fallback!(r::ArrayReg{T}, b::AbstractBlock) where T
     _check_size(r, b)
     r.state .= mat(T, b) * r.state
     return r
