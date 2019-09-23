f1b8310b091ebf7c9e9dabd42dbdaddf95793833
Roger-luo <hiroger@qq.com>
2019-09-23 16:33:19 -0400

    add docs for print_subtypes

--- src/utils.jl
+++ src/utils.jl
@@ -8,7 +8,12 @@ Return projector on `0` or projector on `1`.
 """
 projector(x) = code==0 ? mat(P0) : mat(P1)
 
-function print_subtypetree(t, level=1, indent=4)
+"""
+    print_subtypetree(::Type[, level=1, indent=4])
+
+Print subtype tree, `level` specify the depth of the tree.
+"""
+function print_subtypetree(t::Type, level=1, indent=4)
     level == 1 && println(t)
     for s in subtypes(t)
         println(join(fill(" ", level * indent)) * string(s))
