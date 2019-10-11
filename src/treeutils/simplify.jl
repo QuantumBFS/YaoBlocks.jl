d1be049c54b237023550c53ec9d014c4ccdd0e5e
Roger-luo <hiroger@qq.com>
2019-10-10 23:30:19 -0400

    add simplify

--- src/treeutils/simplify.jl
+++ src/treeutils/simplify.jl
@@ -5,5 +5,27 @@ struct PatternRule{P, T} <: AbstractRule
     term::T
 end
 
+struct Rules
+    rules::Vector{AbstractRule}
+end
+
+Rules(rules...) = Rules(collect(AbstractRule, rules))
+
+function rule(::Val{N}, ::Val{:BASIC}) where N
+    x, y, z = var(N), var(N), var(N)
+    Rules(
+        chain(x, chain(y, z)) => chain(x, y, z),
+        kron(x, kron(y, z)) => kron(x, y, z),
+    )
+end
+
 function simplify(circuit::AbstractBlock)
+end
+
+function simplify(circuit::AbstractBlock, r::PatternRule)
+    m = match(r.pattern, circuit)
+    isempty(m) && return circuit
+
+    d = first(m) # we only use the first match for now
+    replace(r.term, d)
 end
