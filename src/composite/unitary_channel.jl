caa3ad9253b3c3dff4e5d938473396f9553cc407
Roger-luo <hiroger@qq.com>
2019-12-08 19:56:57 -0500

    fix printing & overflow

--- src/composite/unitary_channel.jl
+++ src/composite/unitary_channel.jl
@@ -1,5 +1,18 @@
 using StatsBase
+using YaoArrayRegister
 export UnitaryChannel
+
+# TODO: replace this with uweight
+# when StatsBase 0.33 is out
+_uweights(n) = weights(ones(n))
+
+function check_nqubits(operators)
+    N = nqubits(first(operators))
+    for each in operators
+        N == nqubits(each) || throw(AssertionError("number of qubits mismatch"))
+    end
+    return N
+end
 
 # NOTE: this can have a better support in YaoIR directly with StatsBase
 # as an compiled instruction, but store the weights is the best solution
@@ -29,25 +42,42 @@ julia> UnitaryChannel([X, Y, Z])
 struct UnitaryChannel{N, W <: AbstractWeights} <: CompositeBlock{N}
     operators::Vector{AbstractBlock{N}}
     weights::W
+
+    function UnitaryChannel(operators::Vector)
+        w = _uweights(length(operators))
+        N = check_nqubits(operators)
+        new{N, typeof(w)}(operators, w)
+    end
+
+    function UnitaryChannel(operators::Vector, w::AbstractWeights)
+        N = check_nqubits(operators)
+        new{N, typeof(w)}(operators, w)
+    end
+
+    function UnitaryChannel(operators::Vector, w::AbstractVector)
+        w = weights(w)
+        N = check_nqubits(operators)
+        new{N, typeof(w)}(operators, w)
+    end
 end
 
-# TODO: replace this with uweight
-# when StatsBase 0.33 is out
-function _uweights(n)
-    return weights(ones(n))
-end
-
-UnitaryChannel(ops::Vector) = UnitaryChannel(ops, _uweights(length(ops)))
-UnitaryChannel(it, weights=_uweights(length(it))) = UnitaryChannel(collect(it), weights)
+UnitaryChannel(it, weights) = UnitaryChannel(collect(it), weights)
+UnitaryChannel(it) = UnitaryChannel(collect(it))
 
 function apply!(r::AbstractRegister, x::UnitaryChannel)
     apply!(r, sample(x.operators, x.weights))
 end
 
 function mat(::Type{T}, x::UnitaryChannel) where T
-    sum(x.weights .* mat.(T, x.operators))
+    ws = sum(x.weights)
+    out = x.weights[1] / ws * mat(T, first(x.operators))
+    for k in 2:length(x.operators)
+        out += x.weights[k] / ws * mat(T, x.operators[k])
+    end
+    return out
 end
 
+subblocks(x::UnitaryChannel) = x.operators
 chsubblocks(x::UnitaryChannel{N}, it) where {N} = UnitaryChannel{N}(collect(it), x.weights)
 occupied_locs(x::UnitaryChannel) = union(occupied_locs(x.operators)...)
 
